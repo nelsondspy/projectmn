6502b3019ea167c182b239bd27c0692d343e0b1c
nelsondspy <nelsonds.py@gmail.com>
2014-04-20 14:08:44 -0400

    iteracion de lista de permisos y carga en bdd

--- projectman/projectman/apps/admin/management/commands/db_populate.py
+++ projectman/projectman/apps/admin/management/commands/db_populate.py
@@ -1,22 +1,35 @@
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, User
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand 
-from projectman.apps.admin.models import Proyecto
+from projectman.apps.admin.models import Proyecto, Fase
 from projectman.apps.admin.models import LISTA_PERMISOS
 
 class Command(BaseCommand): 
-    args = '<foo bar ...>'
-    help = 'our help string comes here'
+    args = ''
+    help = ''
     
     def _crea_permisos(self):
+        """
+        
+        inserta los permisos en la base de datos.
+        Los modelos asignados son solo requerimiento del framework. De todas 
+        formas se asigno el modelo acorde a su nivel.  
+        
+        """
         Permission.objects.all().delete()
-        content_type = ContentType.objects.get_for_model( Proyecto)
-        for (idpermiso, descripcion) in LISTA_PERMISOS:
+        
+        for (idpermiso, descripcion, nivel) in LISTA_PERMISOS:
+            if nivel == 0 :
+                content_type = ContentType.objects.get_for_model(User)
+            if nivel == 1 :
+                content_type = ContentType.objects.get_for_model(Proyecto)
+            if nivel == 2:
+                content_type = ContentType.objects.get_for_model(Fase)
+
             Permission.objects.create(codename=idpermiso,
                                        name=descripcion,
                                        content_type=content_type)
 
-    
     def handle(self, *args, **options):
         
         self._crea_permisos()
