87ff863691453f40def45a38d38a57cf560ba002
nelsondspy <nelsonds.py@gmail.com>
2014-04-29 15:28:11 -0400

    Carga y lista de miembros del comite que aprobaran cambios

--- projectman/projectman/apps/gestcambio/views/view_comite.py
+++ projectman/projectman/apps/gestcambio/views/view_comite.py
@@ -1,9 +1,13 @@
 from django.views.generic.edit import CreateView
- 
+from django.views.generic import ListView  
 
 from ..models import ComiteProyecto
 from ..forms import ComiteProyectoForm
+from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse
+from projectman.apps.admin.models import Proyecto
 
+TEMPL_LISTACOMITE ='gestcambio/lista_comite.html'
 
 class CrearComiteProyectoView(CreateView):
     """
@@ -15,3 +19,32 @@ class CrearComiteProyectoView(CreateView):
     models = ComiteProyecto
     form_class = ComiteProyectoForm
     template_name= 'gestcambio/form_comiteproyecto.html'
+    proyecto_ob = None
+    
+    def get_initial(self):
+        self.proyecto_ob = get_object_or_404(Proyecto, pk=self.kwargs['idproyecto'])
+        return { 'proyecto': self.proyecto_ob } # es como hacer #form.idfase=fase
+    
+    def get_success_url(self):
+        return reverse('comite_listar', kwargs={'idproyecto':self.kwargs['idproyecto'] })
+
+    
+    def get_context_data(self, **kwargs):
+        context = CreateView.get_context_data(self, **kwargs) 
+        context['action']= reverse('comite_crear',kwargs={'idproyecto':self.kwargs['idproyecto'] })
+        return context  
+    
+class ListarComiteProyectoView(ListView):
+    models = ComiteProyecto
+    template_name = TEMPL_LISTACOMITE
+    
+    def get_queryset(self):
+        object_list = ComiteProyecto.objects.filter(proyecto_id=self.kwargs['idproyecto'])
+        print "lista comite: "
+        print (object_list)
+        return object_list
+    
+    def get_context_data(self, **kwargs):
+        context = ListView.get_context_data(self, **kwargs)
+        context['idproyecto'] = self.kwargs['idproyecto']
+        return context
