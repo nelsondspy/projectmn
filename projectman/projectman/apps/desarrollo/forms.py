ce610304ff1d9aa77822ec239c36cf0ee6631c34
nelsondspy <nelsonds.py@gmail.com>
2014-04-30 16:27:48 -0400

    formulario de carga de valores, con controles por tipo de dato

--- projectman/projectman/apps/desarrollo/forms.py
+++ projectman/projectman/apps/desarrollo/forms.py
@@ -1,15 +1,16 @@
-
-
 from django.forms import HiddenInput
 from django.forms import IntegerField
-    
- 
+from django.forms import CharField
+from django.forms import ValidationError
+import re
+from datetime import datetime 
 from django.forms import ModelForm 
 from models import ItemTipos
 from models import ItemAtributos
 from models import Item
 from models import ItemAtributosValores
- 
+from models import ItemRelacion
+from projectman.apps.admin.models import  Fase
 
 
 class ItemTiposForm(ModelForm):
@@ -53,10 +54,10 @@ class ItemForm(ModelForm):
         if instance and instance.idfase:
             self.fields['idfase']=IntegerField(widget=HiddenInput)
             self.fields['idfase'].initial=(instance.idfase).idfase
-             
 
 
 class ItemFormN(ModelForm):
+    
     class Meta:
         model = Item
         fields=['nombre', 'idfase','version','idtipoitem']
@@ -64,14 +65,52 @@ class ItemFormN(ModelForm):
 
 
 class ItemValoresForm(ModelForm):
+    
+    tipodato = CharField (widget=HiddenInput())
+    
     def __init__(self, *args, **kwargs):
         super(ItemValoresForm, self).__init__(*args, **kwargs)
         instance = getattr(self,'instance',None)
         if instance :
             self.fields['idatributo'].label=instance.idatributo.__str__()
-            
+            self.fields['tipodato'].initial = instance.idatributo.tipodato
+
+    def clean_valor(self):
+        valor = self.cleaned_data['valor']
+        atributo = self.cleaned_data['idatributo']
+        
+        #valida el valor de tipo de dato numerico
+        #soporta enteros y decimales con signo
+        num_reg = re.compile('(\-|\+)?[0-9]+(\.[0-9])?$')
+        if atributo.tipodato == ItemAtributos.T_NUM:
+            if num_reg.match(valor) is None:
+                raise ValidationError("Ingrese un numero ")
+
+        #valida el valor de un tipo de dato fecha  
+        if atributo.tipodato == ItemAtributos.T_DATE:
+            try:
+                datetime.strptime(valor, "%d/%m/%Y")
+                return valor 
+            except ValueError:
+                raise ValidationError("Ingrese una fecha valida")
+        return valor
         
     class Meta:
         model = ItemAtributosValores
-        fields = ['iditem','idatributo', 'valor']
+        fields = ['iditem', 'idatributo', 'valor']
         widgets ={'iditem':HiddenInput(), 'idatributo':HiddenInput()}
+        
+
+
+class ItemRelacionForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ItemRelacionForm, self).__init__(*args, **kwargs)
+        instance = getattr(self,'instance',None)
+        if instance :
+            items = Item.objects.all()
+            self.fields['origen'].choices = [(item.pk,'['+ item.idfase.__str__()+'] ' + item.nombre\
+                                              ) for item in items]
+    
+    class Meta:
+        model = ItemRelacion
+        fields = ['tipo','origen','destino']
