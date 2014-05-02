3f4584fee61e0ba9327cb23b824c337605223510
nelsondspy <nelsonds.py@gmail.com>
2014-05-02 19:52:29 -0400

    eliminacion de miembros del comite

--- projectman/projectman/apps/gestcambio/urls.py
+++ projectman/projectman/apps/gestcambio/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from views.view_comite import CrearComiteProyectoView ,ListarComiteProyectoView
+from views.view_comite import CrearComiteProyectoView ,ListarComiteProyectoView, EliminarMiembroView
 from views.view_lineabase import CreaLineaBase 
 from views.view_lineabase import ListarLineaBaseView
 
 
 urlpatterns = patterns('projectman.apps',
-    #gestion de tipos de items 
+    #comite 
     url(r'^comite/crear/(?P<idproyecto>\d+)$', login_required(CrearComiteProyectoView.as_view()), name="comite_crear"),
     url(r'^comite/listar/(?P<idproyecto>\d+)$', login_required(ListarComiteProyectoView.as_view()), name="comite_listar"),
+    url(r'^comite/miembro/eliminar/(?P<pk>\d+)$', login_required(EliminarMiembroView.as_view()), name="comite_eliminar"),
+    #linea base
     url(r'^lineabase/crear/(?P<idfase>\d+)$', login_required(CreaLineaBase.as_view()), name='linea_base_item_crear'),
     url(r'^lineabase/listar/(?P<idfase>\d+)$', login_required(ListarLineaBaseView.as_view()), name="lineabase_listar"),
     url(r'^lineabase/listar/(?P<idfase>\d+)/(?P<idlineabase>\d+)$', login_required(ListarLineaBaseView.as_view()), name="lineabase_listardetalle")
--- projectman/projectman/apps/gestcambio/views/view_comite.py
+++ projectman/projectman/apps/gestcambio/views/view_comite.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from django.views.generic import ListView  
 
 from ..models import ComiteProyecto
@@ -6,6 +6,7 @@ from ..forms import ComiteProyectoForm
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from projectman.apps.admin.models import Proyecto
+from django.contrib import messages 
 
 TEMPL_LISTACOMITE ='gestcambio/lista_comite.html'
 
@@ -20,6 +21,7 @@ class CrearComiteProyectoView(CreateView):
     form_class = ComiteProyectoForm
     template_name= 'gestcambio/form_comiteproyecto.html'
     proyecto_ob = None
+    ocurrio_error = False
     
     def get_initial(self):
         self.proyecto_ob = get_object_or_404(Proyecto, pk=self.kwargs['idproyecto'])
@@ -27,14 +29,34 @@ class CrearComiteProyectoView(CreateView):
     
     def get_success_url(self):
         return reverse('comite_listar', kwargs={'idproyecto':self.kwargs['idproyecto'] })
+    
+    def form_valid(self, form):
+        #verifica que solo exista un solo registro del par (usuario, proyecto)
+        miembro = ComiteProyecto.objects.filter(proyecto=form.instance.proyecto, \
+                                                usuario =form.instance.usuario)
+        if miembro.count()>0 :
+            messages.error(self.request, 'ya existe este miembro para este proyecto')
+            self.ocurrio_error = True
+            return self.form_invalid(form)
+        
+        return CreateView.form_valid(self, form)
+    
 
     
     def get_context_data(self, **kwargs):
         context = CreateView.get_context_data(self, **kwargs) 
         context['action']= reverse('comite_crear',kwargs={'idproyecto':self.kwargs['idproyecto'] })
+        if self.ocurrio_error:
+            context['nodefault'] = '__panel.html'
         return context  
-    
+
+
 class ListarComiteProyectoView(ListView):
+    """
+    
+    Lista los miembros del comite del proyecto
+    
+    """
     models = ComiteProyecto
     template_name = TEMPL_LISTACOMITE
     
@@ -48,3 +70,22 @@ class ListarComiteProyectoView(ListView):
         context = ListView.get_context_data(self, **kwargs)
         context['idproyecto'] = self.kwargs['idproyecto']
         return context
+    
+
+class EliminarMiembroView(DeleteView):
+    """
+    
+    Solicita confirmacion para eliminar un miembro del comite
+    
+    """
+    model = ComiteProyecto
+    template_name = 'form_confirm_delete.html'
+    
+    def get_success_url(self):
+        return self.request.META['HTTP_REFERER']
+
+    def get_context_data(self, **kwargs):
+        context = DeleteView.get_context_data(self, **kwargs)
+        context['action'] = reverse('comite_eliminar', kwargs={'pk':self.kwargs['pk']})
+        return context
+
