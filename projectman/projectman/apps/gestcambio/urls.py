14d58c2f042af56e0739fed088ae5c6d6ddb9d39
nelsondspy <nelsonds.py@gmail.com>
2014-05-02 18:23:22 -0400

    vistas de linea base para carga y listado, entradas a url , templates

--- projectman/projectman/apps/gestcambio/urls.py
+++ projectman/projectman/apps/gestcambio/urls.py
@@ -9,7 +9,8 @@ urlpatterns = patterns('projectman.apps',
     #gestion de tipos de items 
     url(r'^comite/crear/(?P<idproyecto>\d+)$', login_required(CrearComiteProyectoView.as_view()), name="comite_crear"),
     url(r'^comite/listar/(?P<idproyecto>\d+)$', login_required(ListarComiteProyectoView.as_view()), name="comite_listar"),
-    url(r'^lineabase/(?P<idfase>\d+)$', login_required(CreaLineaBase.as_view()), name='linea_base_item_crear'),
-    url(r'^lineabase/listar/(?P<idfase>\d+)$', login_required(ListarLineaBaseView.as_view()), name="lineabase_listar")
+    url(r'^lineabase/crear/(?P<idfase>\d+)$', login_required(CreaLineaBase.as_view()), name='linea_base_item_crear'),
+    url(r'^lineabase/listar/(?P<idfase>\d+)$', login_required(ListarLineaBaseView.as_view()), name="lineabase_listar"),
+    url(r'^lineabase/listar/(?P<idfase>\d+)/(?P<idlineabase>\d+)$', login_required(ListarLineaBaseView.as_view()), name="lineabase_listardetalle")
 
 )
--- projectman/projectman/apps/gestcambio/views/view_lineabase.py
+++ projectman/projectman/apps/gestcambio/views/view_lineabase.py
@@ -1,12 +1,12 @@
 
 from django.views.generic import View  
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from ..models import LineaBase
 from ...admin.models import Fase 
 from django.views.generic import ListView 
-
+    
 
 from ..forms import LineaBaseForm
 from ...desarrollo.models import Item
@@ -15,14 +15,14 @@ TEMPL_FORM_LBITEM = 'gestcambio/form_lineabase.html'
 
 class CreaLineaBase(View):
     
-    def get(self, request,idfase):
+    def get(self, request, idfase):
         fase_ob = get_object_or_404(Fase, pk=idfase)
         lineabase = LineaBase()
         lineabase.fase = fase_ob
         form = LineaBaseForm(instance=lineabase)
         #lista solo los items que pertenecen a la fase
-        form.fields['items'].queryset = Item.objects.filter(idfase_id=idfase)
-         
+        form.fields['items'].queryset = Item.objects.filter(idfase_id=idfase).\
+            exclude(estado=Item.E_BLOQUEADO)
         return render(request,TEMPL_FORM_LBITEM,\
                        {'form': form, #'form_itemslb': form_itemslb ,\
                         'action': reverse('linea_base_item_crear', kwargs={'idfase': idfase}) })
@@ -30,13 +30,25 @@ class CreaLineaBase(View):
     def post(self, request, *args, **kwargs):
         form = LineaBaseForm(request.POST)
         if form.is_valid():
+            #guarda la cabecera y el detalle de la linea base
             lineabase = form.save(commit=False)
             lineabase.save()
-            form.save_m2m() # es necesarfio que el padre tenga commit=false  
+            form.save_m2m() # es necesarfio que el padre tenga commit=false 
+            
+            lineabase_pers = LineaBase.objects.get(pk=lineabase.pk)
+            items = lineabase_pers.items.all()
+            
+            #establece el estado de los items selecionados en la lb a bloqueado
+            for item in items:
+                item.estado = Item.E_BLOQUEADO
+                item.save()
         else:
             form = LineaBaseForm()
-        #'action': reverse('linea_base_item_crear', kwargs={'idfase': sefl.kwargs[]}
-        return render(request,TEMPL_FORM_LBITEM, {'form': form })
+            return render(request,TEMPL_FORM_LBITEM, {'form': form })
+        
+        return redirect(reverse('lineabase_listar', \
+                       kwargs={'idfase': self.kwargs['idfase']}))
+
 
 class ListarLineaBaseView(ListView):
     """
@@ -44,20 +56,24 @@ class ListarLineaBaseView(ListView):
     
     """
     model= LineaBase
-    template_name = 'gestcambio/form_listarlineabase.html'
+    template_name = 'gestcambio/lista_lineabase.html'
+    lista_items = None
 
     def get_queryset(self):
-        """
-        busqueda = self.request.GET.get('busqueda','')
-        
-        if (busqueda != ''):
+        #lista solo las lineas bases de la fase 
+        object_list = LineaBase.objects.filter(fase_id=self.kwargs['idfase'])
+        if self.kwargs.get('idlineabase', None):
             
-            object_list = self.model.objects.filter(Q(descripcion__icontains=busqueda) | 
-                                              Q(idlineabase__icontains=busqueda))
-            if object_list.count > 1:
-                messages.info(self.request, 'Resultados con : ' + busqueda)
-        else:
-
-        """
-        object_list = self.model.objects.all()
+            lineabase = get_object_or_404(LineaBase,pk=self.kwargs['idlineabase'])
+            self.lista_items = lineabase.items.all()
+            
+        
         return object_list
+    
+    def get_context_data(self, **kwargs):
+        context = ListView.get_context_data(self, **kwargs)
+        context['idfase'] = self.kwargs['idfase']
+        if self.kwargs.get('idlineabase', None):
+            context['idlineabase'] = int(self.kwargs['idlineabase'])
+        context['itemslb'] = self.lista_items
+        return context
