353be9eab26b5752ba2c63143354f1a75796ee7c
nelsondspy <nelsonds.py@gmail.com>
2014-05-06 18:24:35 -0400

    entradas de url para vista de importa item

--- projectman/projectman/apps/desarrollo/urls.py
+++ projectman/projectman/apps/desarrollo/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from views.view_itemtipo import CreaItemTipoView,EditaItemTipoView, ListaItemTipoView, EliminaItemTipoView
+from views.view_itemtipo import CreaItemTipoView,EditaItemTipoView, ListaItemTipoView,\
+    EliminaItemTipoView , ImporteItemTipoView
 from views.view_itematributo import CreaItemAtributoView, ListaItemAtributoView, EditaItemAtributoView, EliminaItemAtributoView
 from views.view_item import CreaItemView, SetEliminadoItemView, EditItemView
 from views.view_itemvalores import AsignaValoresItem ,ValoreItemView
@@ -25,6 +26,8 @@ urlpatterns = patterns('projectman.apps',
     url(r'^tipoitem/lista/(?P<idfase>\d+)$', login_required(ListaItemTipoView.as_view()), name="tipoitem_lista"),
     url(r'^tipoitem/attr/(?P<idfase>\d+)/(?P<idtipoitem>\d+)$', login_required(ListaItemTipoView.as_view()), name="tipoitem_attr"),
     url(r'^tipoitem/eliminar/(?P<pk>\d+)$', login_required(EliminaItemTipoView.as_view()), name="tipoitem_eliminar"),
+    url(r'^tipoitem/importar/(?P<idfase>\d+)$', login_required(ImporteItemTipoView.as_view()), name='tipoitem_importar'),
+    
     #gestion de atributos
     url(r'^atributos/crear/(?P<idtipoitem>\d+)$', login_required(CreaItemAtributoView.as_view()),name="itematributo_crear"),
     url(r'^atributos/editar/(?P<pk>\d+)$', login_required(EditaItemAtributoView.as_view()),name="itematributo_editar"),
