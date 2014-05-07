1ecb6edb730aae96e858082e559241b731389f0f
nelsondspy <nelsonds.py@gmail.com>
2014-05-07 00:52:40 -0400

    no permite establecer como eliminado el padre de una relacion

--- projectman/projectman/apps/desarrollo/views/view_item.py
+++ projectman/projectman/apps/desarrollo/views/view_item.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages 
 from django.views.generic.edit import CreateView  , UpdateView
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from django.forms.widgets import HiddenInput
+from django.forms import Form
 #modelos 
 from projectman.apps.admin.models import  Fase
 from ..models import Item , ItemTipos
+from ..models import ItemRelacion
 #formularios
 from ..forms import ItemForm, ItemFormN
 from view_oth import redirige_edicion_actual
@@ -103,6 +106,8 @@ class SetEliminadoItemView(UpdateView):
     """
     
     Vista que permite cambiar es estado a un item a eliminado.
+    Tambien establece como eliminado el estado de las relaciones que dependen del item.
+    Si el item a eliminar es padre de alguna relacion se deniega el cambio de estado a eliminado
     
     """
     model = Item
@@ -110,16 +115,39 @@ class SetEliminadoItemView(UpdateView):
     
     def post(self, request, *args, **kwargs):
         #establece el estado de item  a eliminado
+        
         item_eliminar = get_object_or_404(Item, pk=self.kwargs['pk'])
+            #si no paso alguna validacion
+        
         item_eliminar.estado = Item.E_ELIMINADO
         item_eliminar.save()
         
         return redirect(get_url_edicion_actual(request, nivel=1))
 
     def get_context_data(self, **kwargs):
+        
         context = UpdateView.get_context_data(self, **kwargs)
+        #ejecuta una serie de validaciones para permitir establecer a eliminado
+        #o ocultar el formulario
+        item_eliminar = get_object_or_404(Item, pk=self.kwargs['pk'])
+        if not self.eliminacion_valida(item_eliminar):
+            context['denegar_eliminacion'] = True
+
         context['action'] = reverse('item_eliminar' , kwargs={'pk':self.kwargs['pk']} )
         return context
+    
+    def eliminacion_valida(self, item):
+        if self.item_espadre(item):
+            messages.error(self.request, 'El item a eliminar es padre o antecesor de al menos un item' )
+            return False
+        return True
+
+    def item_espadre(self, item):
+        """
+        devuelve true si el item es padre de otro
+        
+        """
+        return ItemRelacion.objects.filter(origen=item).count() > 0
 
 
 class EditItemView(UpdateView):
@@ -155,7 +183,8 @@ class EditItemView(UpdateView):
         #por el momento no esta permitido el cambio de tipo de item
         form.fields['idtipoitem'].widget = HiddenInput()
         return form
-    
+
+
 class ListaEliminadosView(ListView):
     """ lista los items eliminados """
     model = Item
@@ -168,4 +197,4 @@ class ListaEliminadosView(ListView):
         """
         object_list = Item.objects.filter(estado=Item.E_ELIMINADO)
         return object_list
-    
+
