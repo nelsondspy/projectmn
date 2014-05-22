78776895cd5eb5fe3ce04949caf7db4effde89e9
nelsondspy <nelsonds.py@gmail.com>
2014-05-22 15:42:52 -0400

    metodo que devuelve el estado actual de la votacion de una solicitud

--- projectman/projectman/apps/gestcambio/views/view_solicitudvoto.py
+++ projectman/projectman/apps/gestcambio/views/view_solicitudvoto.py
@@ -3,12 +3,13 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from django.views.generic import ListView
-
+from django.db.models import Count
 from django.contrib import messages
 from ..models import SolicitudCambio, SolicitudVoto, ComiteProyecto
 from django.db.models import Q
 from django.http import Http404
 
+from view_comite import  CrearComiteProyectoView
 class ListaSolicPendientes(ListView):
     
     template_name = 'gestcambio/lista_solic_pendientes.html'
@@ -34,6 +35,7 @@ class ListaSolicPendientes(ListView):
         context['E_ENVIADO'] = SolicitudCambio.E_ENVIADO
         context['E_TERMINADO'] = SolicitudCambio.E_TERMINADO
         context['E_BORRADOR'] = SolicitudCambio.E_BORRADOR
+        
 
         return context
 
@@ -47,7 +49,8 @@ class VotaSolicitudView(View):
         """
 
         Despliega el formulario de confirmacion generico.
-        Con valores particulares para confirmar la aprobacion o el rechazo
+        Con valores particulares para confirmar el voto de aprobacion o el rechazo de
+        una solicitud de cambio.
 
         """
         if not accion in self.ACCIONES_POSIBLES:
@@ -66,6 +69,10 @@ class VotaSolicitudView(View):
                                              'texto': mensaje ,\
                                              'value': 'Aceptar' })
     def post(self,request, *args, **kwargs):
+        """
+        Guarda el nuevo voto.
+        
+        """
         accion = self.kwargs['accion']
         pk = self.kwargs['pk']
         if not accion in self.ACCIONES_POSIBLES:
@@ -74,11 +81,31 @@ class VotaSolicitudView(View):
         solicitud = get_object_or_404(SolicitudCambio, pk=pk)
         proyecto_id = solicitud.lineabase.fase.idproyecto_id
         miembro = get_object_or_404(ComiteProyecto, proyecto_id=proyecto_id, usuario=request.user )
-        
+        #nueva instancia de voto
         voto = SolicitudVoto()
         voto.solicitud=solicitud
         voto.miembro=miembro
         voto.aprobado = (accion==self.APROBAR) 
         voto.save()
-        messages.success(request,'Voto realizado exitosamente')
+        messages.success(request,'Voto enviado exitosamente')
         return redirect( reverse('solicitudes_pend_proy',kwargs={'idproyecto':proyecto_id}))
+
+    @classmethod
+    def estado_votacion(self, idsolicitud ):
+        """
+        
+        Metodo que determina el estado actual de la votacion para una solicitud.
+        retorna una tupla de la siguiente estructura (votos a favor, votos en contra, votos faltantes )
+        
+        """
+        votos_sol = SolicitudVoto.objects.filter(solicitud_id=idsolicitud)
+        def prefvotos(x) : votos_sol.filter(aprobado=x).count()
+        votos_favor = prefvotos(True)
+        votos_contra = prefvotos(False)
+        #obtenemos la cantidad de votos esperados que es  
+        solicitud = get_object_or_404(SolicitudCambio, pk=idsolicitud)
+        proyecto_id = solicitud.lineabase.fase.idproyecto_id
+        (miembros, ) = CrearComiteProyectoView.miembros_proyecto(proyecto_id)
+        vot_faltantes = miembros.count() - ( votos_favor + votos_contra )
+        return (votos_favor, votos_contra, vot_faltantes)
+    
