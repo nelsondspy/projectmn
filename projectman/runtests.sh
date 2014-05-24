56b99f11d04fd0fd5e332ec446db3bb9e86cfb8c
fabir <fabismith07@gmail.com>
2014-05-24 00:25:19 -0400

    Script de ejecución de tests mosdificado

--- projectman/runtests.sh
+++ projectman/runtests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python manage.py test projectman.apps.admin.tests
+python manage.py test projectman.apps.desarrollo.tests
+python manage.py test projectman.apps.gestcambio.tests
 read -p "Presione enter para cerrar .."
