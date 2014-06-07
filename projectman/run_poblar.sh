3c1b0dc8328f2c2a9ee626ef22286051f5206588
nelsondspy <nelsonds.py@gmail.com>
2014-06-07 01:40:36 -0400

    cambios menores en script

--- projectman/run_poblar.sh
+++ projectman/run_poblar.sh
@@ -2,6 +2,6 @@
 echo "sincronizando estructura.."
 python manage.py syncdb
 echo "poblando base de datos .."
-python manage.py loaddata datadump_1.json
+python manage.py loaddata datadump_2.json
 echo "..Listo"
 read -p "Presione enter para cerrar .."
